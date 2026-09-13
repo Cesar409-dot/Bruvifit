c8d3c84dad9d18b2de9ad8d2b8a7b875ba66cc56
RafaelSouza115 <rafael.asantos@sptech.school>
2026-09-13 02:20:02 -0300

    feat: estrutura inicial do sistema BRUVIFIT

---
+++ backend/bruvifit_app/src/main/java/school/sptech/bruvifit_app/BruvifitAppApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.bruvifit_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruvifitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BruvifitAppApplication.class, args);
+	}
+
+}
---
+++ backend/bruvifit_app/src/test/java/school/sptech/bruvifit_app/BruvifitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.bruvifit_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BruvifitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
